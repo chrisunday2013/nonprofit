0da7074979de6c9e89248b16c012fac0eab56e28
Christian Sunday <potentialsunny47@gmail.com>
2023-01-31 08:42:51 +0530

    lots of updates

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Counter
 # Register your models here.
+
+
+
+class CounterAdmin(admin.ModelAdmin):
+    list_display=('countries', 'volunteers', 'our_goal', 'raised' )
+admin.site.register(Counter, CounterAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User 
+
+
+# home
+class Counter(models.Model):
+    countries=models.IntegerField()
+    volunteers=models.IntegerField()
+    our_goal=models.IntegerField()
+    raised=models.IntegerField()
+
+        
+    def __str__(self):
+        return self.countries
+
+class Events(models.Model):
+    title=models.CharField(max_length= 50, null=False )
+    content=models.TextField(max_length=400, null=False)
+    date=models.DateTimeField()
+    venue=models.CharField(max_length=40)
