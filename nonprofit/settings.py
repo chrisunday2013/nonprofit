765a7197f4cbe654ec66d5b3f6cca5a5b7734388
Christian Sunday <potentialsunny47@gmail.com>
2023-02-02 00:30:32 +0530

    fix staticfiles

--- nonprofit/settings.py
+++ nonprofit/settings.py
@@ -130,13 +130,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [BASE_DIR / "static",]
 
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
