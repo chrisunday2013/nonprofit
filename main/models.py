d7eaf661b65376f992a4e5f57bf95b697a941a3f
Christian Sunday <potentialsunny47@gmail.com>
2023-02-01 05:39:35 +0530

    added models

--- main/models.py
+++ main/models.py
@@ -19,3 +19,50 @@ class Events(models.Model):
     content=models.TextField(max_length=400, null=False)
     date=models.DateTimeField()
     venue=models.CharField(max_length=40)
+    image = models.ImageField(default="default.jpg", upload_to='portfolio_pics' )
+
+
+    def __str__(self):
+        return self.title
+
+
+class Team(models.Model):
+    image = models.ImageField(default="default.jpg", upload_to='portfolio_pics' )
+    full_name = models.CharField(max_length=100, null=False)
+    position = models.CharField(max_length=50, null=False)
+
+
+    def __str__(self):
+        return self.full_name
+
+
+class Volunteer(models.Model):
+    full_name = models.CharField(max_length=100, null=False)   
+    email= models.EmailField(max_length=100, null=True)  
+    reason_for_joining= models.TextField(max_length=500, null=False)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Testimonial(models.Model):
+    full_name = models.CharField(max_length=100, null=False) 
+    profession = models.CharField(max_length=50, null=False)
+    testimony= models.TextField(max_length=500, null=False)
+
+
+
+class Contact(models.Model):
+    full_name=models.CharField(max_length=100, null=True)
+    email=models.EmailField(max_length=100, null=True)
+    subject=models.CharField(max_length=100, null=True)
+    message=models.TextField(null=True)
+
+    def __str__(self):
+        return self.full_name
+
+
+
+
+
+
