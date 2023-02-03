8a8fb65070bcf331a8f35d83721be36a894c2090
Christian Sunday <potentialsunny47@gmail.com>
2023-02-03 13:23:15 +0530

    removed vercel setup

--- nonprofit/urls.py
+++ nonprofit/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += staticfiles(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
