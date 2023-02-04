c7d7d6ddf19eefcf54857241193a57f5047fef32
Christian Sunday <potentialsunny47@gmail.com>
2023-02-04 06:25:42 +0530

    collected staticfiles

--- nonprofit/urls.py
+++ nonprofit/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings 
-from django.conf.urls.static import staticfiles
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -11,4 +11,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += staticfiles(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
