f0ee933d0a92e7d2a163b5f33e78805756d21430
Christian Sunday <potentialsunny47@gmail.com>
2023-03-28 08:46:14 +0100

    database

--- nonprofit/settings.py
+++ nonprofit/settings.py
@@ -74,25 +74,25 @@ WSGI_APPLICATION = 'nonprofit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'b4GVMs9U6XIqRrXh1DBA',
-#         'HOST': 'containers-us-west-188.railway.app',
-#         'PORT': '7846',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'b4GVMs9U6XIqRrXh1DBA',
+        'HOST': 'containers-us-west-188.railway.app',
+        'PORT': '7846',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
