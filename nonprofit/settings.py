b07f34a395e051b4800b2c3f86453d2eaabffe81
Christian Sunday <potentialsunny47@gmail.com>
2023-05-24 02:18:50 +0100

    added core allowed origin

--- nonprofit/settings.py
+++ nonprofit/settings.py
@@ -21,7 +21,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://nneomafoundation.org/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-bbd4.up.railway.app/']
 # CSRF_COOKIE_DOMAIN = ['https://nneomafoundation.org/']
 
 CORS_ALLOWED_ORIGINS = [
